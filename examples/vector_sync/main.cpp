f2bacab5cd8ab714d5e7b1bfa89ebafd43b67d7c
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2025-06-12 23:19:00 +0300

    refac: change variable names to better clearity

--- examples/vector_sync/main.cpp
+++ examples/vector_sync/main.cpp
@@ -3,9 +3,11 @@
 #include "example.hpp"
 
 int main() {
-  constexpr std::size_t repeat_n{5};
-  Monitor<std::vector<std::string>> mon;
-  std::jthread writer{ex::write_task, std::ref(mon), "writer", repeat_n};
-  std::jthread reader{ex::read_task, std::ref(mon), "reader", repeat_n};
+  constexpr std::size_t repeat_ntimes{5};
+  Monitor<std::vector<std::string>> monitor;
+  std::jthread writer{ex::write_task, std::ref(monitor), "writer",
+                      repeat_ntimes};
+  std::jthread reader{ex::read_task, std::ref(monitor), "reader",
+                      repeat_ntimes};
   return 0;
 }
